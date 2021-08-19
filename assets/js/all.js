bab49908d7c157a9170ef83701802d92c6d71aa7
a733181 <a733181@gmail.com>
2021-08-19 16:14:09 +0800

    Update 2021-08-19T08:13:55.557Z

--- assets/js/all.js
+++ assets/js/all.js
@@ -1,6 +1,9 @@
+"use strict";
+
 var swiper = new Swiper(".mySwiper", {
   slidesPerView: 4,
   spaceBetween: 30,
   initialSlide: 1,
-  centeredSlides: true,
+  centeredSlides: true
 });
+//# sourceMappingURL=all.js.map
